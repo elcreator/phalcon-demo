c02938bf16c35c4f3addb3d2ee835c19b039b65f
Artur Kyryliuk <artur.kyryliuk@synapse.com>
2022-12-04 21:09:54 +0100

    Updated to Ubuntu 22 LTS and PHP 8.1, Phalcon 5.1.2

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,7 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/focal64"
+  config.vm.box = "ubuntu/jammy64"
   config.vm.provider "virtualbox" do |v|
     v.memory = 4096
     v.cpus = 4
--- install.sh
+++ install.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
 set -x
 DOMAIN=project.local
-PHP_VER=7.4
-PHP_BUILD=20190902
-PHALCON_VER=v5.0.4
-XDEBUG_VER=2.9.8
+PHP_VER=8.1
+PHP_BUILD=20210902
+PHALCON_VER=v5.1.2
 IDE_KEY=PHPSTORM
 VM_IP=192.168.56.37
 VM_GW=192.168.56.1
-
-sudo add-apt-repository -y ppa:ondrej/php
-sudo add-apt-repository -y ppa:ondrej/nginx-mainline
-echo "deb https://nginx.org/packages/mainline/ubuntu/ focal nginx" | sudo tee -a /etc/apt/sources.list
-echo "deb-src https://nginx.org/packages/mainline/ubuntu/ focal nginx" | sudo tee -a /etc/apt/sources.list
-wget http://nginx.org/keys/nginx_signing.key
-sudo apt-key add ./nginx_signing.key
 
 sudo apt update
 sudo apt upgrade
@@ -23,6 +15,13 @@ sudo apt install -y gcc
 sudo apt install -y make
 sudo apt install -y nginx
 sudo adduser --system --no-create-home --shell /bin/false --group --disabled-login nginx
+
+sudo update-alternatives --set php /usr/bin/php${PHP_VER}
+sudo update-alternatives --set phar /usr/bin/phar${PHP_VER}
+sudo update-alternatives --set phar.phar /usr/bin/phar.phar${PHP_VER}
+sudo update-alternatives --set phpize /usr/bin/phpize${PHP_VER}
+sudo update-alternatives --set php-config /usr/bin/php-config${PHP_VER}
+
 sudo apt install -y php${PHP_VER}-fpm
 sudo apt install -y php${PHP_VER}-cli
 sudo apt install -y php${PHP_VER}-psr
@@ -36,14 +35,8 @@ sudo apt install -y php${PHP_VER}-mysql
 sudo apt install -y php${PHP_VER}-sqlite3
 sudo apt install -y php${PHP_VER}-xml
 sudo apt install -y php${PHP_VER}-zip
+sudo apt install -y php${PHP_VER}-xdebug
 
-sudo update-alternatives --set php /usr/bin/php${PHP_VER}
-sudo update-alternatives --set phar /usr/bin/phar${PHP_VER}
-sudo update-alternatives --set phar.phar /usr/bin/phar.phar${PHP_VER}
-sudo update-alternatives --set phpize /usr/bin/phpize${PHP_VER}
-sudo update-alternatives --set php-config /usr/bin/php-config${PHP_VER}
-
-sudo apt install -y composer
 sudo apt-get install -y wget
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password admin'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password admin'
@@ -52,9 +45,17 @@ sudo apt install -y mysql-server
 sudo apt install -y mysql-client
 
 sudo apt install -y htop
+sudo apt install -y unzip
 sudo apt install -y net-tools
 
 cd /tmp || exit
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+
 wget http://pear.php.net/go-pear.phar
 php go-pear.phar
 
@@ -67,22 +68,11 @@ sudo phpenmod -v ${PHP_VER} apcu
 wget https://github.com/phalcon/cphalcon/releases/download/${PHALCON_VER}/phalcon-pecl.tgz
 pear install phalcon-pecl.tgz
 
-wget http://xdebug.org/files/xdebug-${XDEBUG_VER}.tgz
-tar -xvzf xdebug-${XDEBUG_VER}.tgz
-cd xdebug-${XDEBUG_VER} || exit
-phpize
-./configure
-make
-sudo cp modules/xdebug.so /usr/lib/php/${PHP_BUILD}
-cd /tmp || exit
-
 echo '; priority=50' | sudo tee -a /etc/php/${PHP_VER}/mods-available/xdebug.ini
 echo "zend_extension = /usr/lib/php/${PHP_BUILD}/xdebug.so" | sudo tee -a /etc/php/${PHP_VER}/mods-available/xdebug.ini
-echo 'xdebug.remote_autostart = 1' | sudo tee -a /etc/php/${PHP_VER}/mods-available/xdebug.ini
-echo 'xdebug.remote_enable = 1' | sudo tee -a /etc/php/${PHP_VER}/mods-available/xdebug.ini
-echo 'xdebug.extended_info = 1' | sudo tee -a /etc/php/${PHP_VER}/mods-available/xdebug.ini
+echo 'xdebug.mode = debug' | sudo tee -a /etc/php/${PHP_VER}/mods-available/xdebug.ini
+echo 'xdebug.start_with_request = trigger' | sudo tee -a /etc/php/${PHP_VER}/mods-available/xdebug.ini
 echo "xdebug.remote_host = \"${VM_GW}\"" | sudo tee -a /etc/php/${PHP_VER}/mods-available/xdebug.ini
-echo 'xdebug.remote_mode = "req"' | sudo tee -a /etc/php/${PHP_VER}/mods-available/xdebug.ini
 echo "xdebug.idekey = \"${IDE_KEY}\"" | sudo tee -a /etc/php/${PHP_VER}/mods-available/xdebug.ini
 
 sudo adduser --quiet --disabled-password --shell /bin/bash --home /home/project --gecos "project" project
@@ -91,7 +81,8 @@ echo "project:project" | chpasswd
 sudo cp -r /vagrant/* /home/project/
 
 sudo ln -s /home/project/config/php-fpm/project.conf /etc/php/${PHP_VER}/fpm/pool.d/project.conf
-sudo ln -s /home/project/config/php-fpm/phalcon.ini /etc/php/${PHP_VER}/fpm/conf.d/50-phalcon.ini
+sudo cp /home/project/config/php-fpm/phalcon.ini /etc/php/${PHP_VER}/mods-available/
+sudo phpenmod -v ${PHP_VER} phalcon
 sudo mkdir -p /etc/nginx/sites-enabled
 sudo rm -f /etc/nginx/nginx.conf
 sudo cp /home/project/config/nginx/nginx.conf /etc/nginx/
